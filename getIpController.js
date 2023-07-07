f91829bd8b03d8f6f9dccd94d2bc810c98a41631
markhryshchenko <grishenko.ua@gmail.com>
2023-07-07 17:23:50 +0300

    test 4 of ip

--- getIpController.js
+++ getIpController.js
@@ -3,29 +3,29 @@ const fs = require("fs");
 const results = [];
 //const ip = '167.78.240';
 
-fs.createReadStream("data.csv")
-  .pipe(csv())
-  .on("data", (data) => results.push(data))
-  .on("end", () => {});
-
 class getIpController {
   async getUser(req, res) {
     try {
+      fs.createReadStream("data.csv")
+        .pipe(csv())
+        .on("data", (data) => results.push(data))
+        .on("end", () => {});
+
       let ip_adress =
         (req.headers["x-forwarded-for"] || "").split(",").pop() ||
         req.headers["x-real-ip"] ||
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress;
-       
-        let ip_ = ip_adress.replace(/\./g, "");
-      
-        res.status(200).json(ip_);
+
+      let ip_ = ip_adress.replace(/\./g, "");
+
+      res.status(200).json(ip_);
       let index = results.findIndex(
         (el) => ip_ >= Number(el.from) && ip_ <= Number(el.to)
       );
-     
-      console.log(results[index])
+
+      console.log(results[index]);
       res.status(200).json(`your IP: ${results[index]}`);
     } catch (e) {
       console.log(e);
